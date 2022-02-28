71284e01068e39da489bc02b36f41dd7631f7244
Adrian <adrianhenry2115@gmail.com>
2022-02-28 18:38:22 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -20,13 +20,7 @@ function filterByQuery(query, animalsArray) {
         }
         // Loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-            // Check the trait against each animal in the filteredResults array.
-            // Remember, it is initially a copy of the animalsArray,
-            // but here we're updating it for each trait in the .forEach() loop.
-            // For each trait being targeted by the filter, the filteredResults
-            // array will then contain only the entries that contain the trait,
-            // so at the end we'll have an array of animals that have every one 
-            // of the traits when the .forEach() loop is finished.
+
             filteredResults = filteredResults.filter(
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
