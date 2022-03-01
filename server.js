74c3cb576ddd9fc8b2c6ca7b6ce7111db03bfe7a
Adrian <adrianhenry2115@gmail.com>
2022-03-01 16:27:47 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -43,6 +50,14 @@ function filterByQuery(query, animalsArray) {
 function findByID(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+};
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    // return finished code to post route for response
+    return animal;
 }
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -59,6 +74,17 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404)
     }
+});
+
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // add animal to json file and animalsArray in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animals);
 });
 
 app.listen(PORT, () => {
