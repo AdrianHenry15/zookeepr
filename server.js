2b0bd78fe3c12b74e255e326942f2bb679420b56
Adrian <adrianhenry2115@gmail.com>
2022-02-28 19:21:06 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const { animals } = require('./data/animals');
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
